f4297fa0e50c15fe93dae746214352dc48d458a5
APotatoChip <cvetina.a.m@gmail.com>
2021-12-12 02:50:55 +0200

    edit implementation and half of details

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -18,19 +18,42 @@ module.exports = {
             res.render('./housing/create.hbs');
         },
         details(req, res, next) {
-
-            res.render('./housing/details.hbs');
+            House
+                .findOne({ _id: req.params.houseId })
+                .lean()
+                .then((house) => {
+                    res.render('./housing/details.hbs', {...house });
+                })
         },
         edit(req, res, next) {
+            House
+                .findOne({ _id: req.params.houseId })
+                .then((house) => {
 
-            res.render('./housing/edit.hbs');
+                    res.render('./housing/edit.hbs', house);
+                });
         },
+        delete(req, res, next) {
+            House
+                .deleteOne({ _id: req.params.houseId })
+                .then((result) => {
+                    res.redirect('/housing/all');
+                });
+        }
     },
     post: {
         create(req, res, next) {;
             House.create({...req.body, owner: req.user._id })
                 .then((createdHouse) => {
                     res.redirect('/housing/all');
+                });
+        },
+        edit(req, res, next) {
+            const { houseId } = req.params;
+            House
+                .updateOne({ _id: req.params.houseId }, { $set: {...req.body } })
+                .then((updatedHouse) => {
+                    res.redirect(`/housing/details/${houseId}`);
                 });
         }
     }
--- routers/housingRouter.js
+++ routers/housingRouter.js
@@ -4,10 +4,12 @@ module.exports = (router) => {
 
     router.get('/all', houseController.get.all);
     router.get('/create', houseController.get.create);
-    router.get('/details', houseController.get.details);
-    router.get('/edit', houseController.get.edit);
+    router.get('/details/:houseId', houseController.get.details);
+    router.get('/edit/:houseId', houseController.get.edit);
+    router.get('/delete/:houseId', houseController.get.delete);
 
     router.post('/create', houseController.post.create);
+    router.post('/edit/:houseId', houseController.post.edit);
 
     return router;
 }
